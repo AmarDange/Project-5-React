e1fa03037fcb867f87815c4ec8e8afce2e18dce8
AmarDange <amardange218@gmail.com>
2023-06-04 17:29:59 +0000

    message

---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1> Hello from react! </h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1> Hello  </h1>;
+
+export default FunctionalGreetingWithProps;
