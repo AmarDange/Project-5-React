7bff062b6ed0d2e2cb787a923717e4c198d96cb8
AmarDange <amardange218@gmail.com>
2023-06-10 16:31:25 +0000

    adding components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+/// StatefulGreeting///
+
+// import logo from './logo.svg';
+// import './App.css';
+// import StatefulGreeting from './components/StatefulGreeting';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+///  StatefulGreetingWithCallback ///
+
+// import logo from "./logo.svg";
+// import "./App.css";
+// import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StatefulGreetingWithCallback />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+//// StatefulGreetingWithPrevState ///
+
+// import "./App.css";
+// import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <StatefulGreetingWithPrevState />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+// import "./App.css";
+// import EventsClass from "./components/EventsClass";
+// import EventsFunctional from "./components/EventsFunctional";
+
+// function App() {
+//   return <div className="App">
+//       <EventsFunctional />
+//       <EventsClass />
+//   </div>;
+// }
+
+// export default App;
+
+
+
+// import "./App.css";
+// import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+// import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <ConditionalRenderingClass />
+//       <ConditionalRenderingFunctional connected={true} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import "./App.css";
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
-  return (
-    <div className="App">
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32"/>
-    </div>
-  );
+  return <div className="App">
+      < NestingComponents />
+  </div>;
 }
 
 export default App;
