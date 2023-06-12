e4397bb09859ebc520126d1f88603e601db713d6
AmarDange <amardange218@gmail.com>
2023-06-12 07:51:09 +0000

    adding forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -86,12 +86,152 @@
 
 
 
+// import "./App.css";
+// import NestingComponents from "./components/NestingComponents";
+
+// function App() {
+//   return <div className="App">
+//       < NestingComponents />
+//   </div>;
+// }
+
+// export default App;
+
+
+
+// import "./App.css";
+// import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+
+// function App() {
+//   return <div className="App">
+//       <MethodsAsPropsParent />
+//   </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import RenderingLists from "./components/RenderingLists";
+
+// function App() {
+//   return <div className="App">
+//       <RenderingLists />
+//   </div>;
+// }
+
+// export default App;
+
+
+
+// import "./App.css";
+// import Clock from "./components/Clock";
+
+// function App() {
+//   return <div className="App">
+//       <Clock />
+//     </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import LifeCyclesCDM from "./components/LifeCyclesCDM";
+
+// function App() {
+//   return <div className="App">
+//       <LifeCyclesCDM />
+//   </div>;
+// }
+
+// export default App;
+
+
+
+
+// import "./App.css";
+// import LifeCyclesCDU from "./components/LifeCyclesCDU";
+
+// function App() {
+//   return <div className="App">
+//       <LifeCyclesCDU />
+//   </div>;
+// }
+
+// export default App;
+
+
+
+// import "./App.css";
+// import LifeCyclesCWU from "./components/LifeCyclesCWU";
+
+// function App() {
+//   return <div className="App">
+//       <LifeCyclesCWU />
+//     </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import ControlledForm from "./components/ControlledForm";
+
+// function App() {
+//   return <div className="App">
+//       <ControlledForm />
+//   </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import UncontrolledForm from "./components/UncontrolledForm";
+
+// function App() {
+//   return <div className="App">
+//       <UncontrolledForm />
+//   </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import { SearchBar3 } from "./components/SearchBar3";
+
+// function App() {
+//   return <div className="App">
+//       <SearchBar3 />
+//   </div>;
+// }
+
+// export default App;
+
+
+// import "./App.css";
+// import HooksCounter from "./components/HooksCounter";
+// import ClassCounter from "./components/ClassCounter";
+
+// function App() {
+//   return <div className="App">
+//       <HooksCounter />
+//       <ClassCounter />
+//     </div>;
+// }
+
+// export default App;
+
+
+
 import "./App.css";
-import NestingComponents from "./components/NestingComponents";
+import ControlledFormHooks from "./components/ControlledFormHooks";
 
 function App() {
   return <div className="App">
-      < NestingComponents />
+      <ControlledFormHooks />
   </div>;
 }
 
